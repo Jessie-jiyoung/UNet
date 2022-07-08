6a6e8044c0517805fc8d668f5b81ed9039c96666
Jessie-jiyoung <jessie.lee@viewmagine.com>
2022-07-08 13:21:09 +0900

    UNet Created

---
+++ train.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from torchvision import transforms, datasets
+
+lr = 1e-3
+batch_size = 4
+num_epoch = 100
+
+data_dir = './datasets'
+ckpt_dir = './checkpoint'
+log_dir = './log'
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
